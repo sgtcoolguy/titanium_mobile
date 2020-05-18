840ca9eebfc39d9912b622d4f7b011eff5502f27
Chris Williams <chris.a.williams@gmail.com>
2020-05-18 14:15:11 -0400

    refactor(android): move most js ti extensions to common

---
+++ common/Resources/ti.internal/extensions/ti/ui/activityindicator.js
@@ -0,0 +1,9 @@
+/* globals OS_ANDROID */
+
+if (OS_ANDROID) {
+	// Define constants for ActivityIndicator here for now.
+	Titanium.UI.ActivityIndicator.STATUS_BAR = 0;
+	Titanium.UI.ActivityIndicator.DIALOG = 1;
+	Titanium.UI.ActivityIndicator.INDETERMINANT = 0;
+	Titanium.UI.ActivityIndicator.DETERMINANT = 1;
+}
---
+++ common/Resources/ti.internal/extensions/ti/ui/index.js
@@ -0,0 +1,10 @@
+import './activityindicator';
+import './iphone';
+import './listview';
+import './navigationwindow';
+import './semanticColor';
+import './tab';
+import './tabgroup';
+import './view';
+import './webview';
+import './window';
