08c3924df7bc1cc0a787b5475b6dd1010a83c256
Chris Williams <chris.a.williams@gmail.com>
2020-08-31 14:45:12 -0400

    fix(ios): implement Ti.Blob.arrayBuffer natively

--- common/Resources/ti.internal/extensions/ti/ti.blob.js
+++ common/Resources/ti.internal/extensions/ti/ti.blob.js
@@ -4,23 +4,26 @@
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
-const buffer = Ti.createBuffer({ value: '' });
-const blob = buffer.toBlob();
-const BlobPrototype = Object.getPrototypeOf(blob);
-// FIXME: This doesn't "stick" for iOS. Probably need to implement it natively.
-// Web Blob has an arrayBuffer() method that returns a Promise
-// https://developer.mozilla.org/en-US/docs/Web/API/Blob/arrayBuffer
-Object.defineProperty(BlobPrototype, 'arrayBuffer', {
-	value: function () {
-		return new Promise((resolve, reject) => {
-			let buf;
-			try {
-				buf = this.toArrayBuffer();
-			} catch (err) {
-				return reject(err);
-			}
-			resolve(buf);
-		});
-	},
-	enumerable: true
-});
+/* globals OS_ANDROID */
+if (OS_ANDROID) {
+	const buffer = Ti.createBuffer({ value: '' });
+	const blob = buffer.toBlob();
+	const BlobPrototype = Object.getPrototypeOf(blob);
+	// FIXME: This doesn't "stick" for iOS. Probably need to implement it natively.
+	// Web Blob has an arrayBuffer() method that returns a Promise
+	// https://developer.mozilla.org/en-US/docs/Web/API/Blob/arrayBuffer
+	Object.defineProperty(BlobPrototype, 'arrayBuffer', {
+		value: function () {
+			return new Promise((resolve, reject) => {
+				let buf;
+				try {
+					buf = this.toArrayBuffer();
+				} catch (err) {
+					return reject(err);
+				}
+				resolve(buf);
+			});
+		},
+		enumerable: true
+	});
+}
